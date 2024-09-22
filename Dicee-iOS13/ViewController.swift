8f4d0a38d020f11ccfb0f5b232fd62b3829dc9a4
kanishka <vs7894182@gmail.com>
2024-09-22 14:54:54 +0530

    git push

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBoutlet lets you reference a UI element in main.storyboard
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceSix")
     }
 
-
+    @IBAction func buttonpressed(_ sender: UIButton) {
+        let array = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+        diceImageView1.image = array[Int.random(in: 0...5)]
+        diceImageView2.image = array[Int.random(in: 0...5)]
+        
+    }
+    
 }
 
